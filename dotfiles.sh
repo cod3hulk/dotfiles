fe4872422466c162d6be4e774dba4da3a7b89566
Tomas Ave <tomave82@gmail.com>
2014-11-18 08:10:32 +0100

    Add brew cask formulas

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,6 +2,7 @@
 set -e
 
 INSTALLED_FORMULAS=$(brew list -1)
+INSTALLED_CASK_FORMULAS=$(brew cask list -1)
 
 print_banner() {
     echo '      .___      __    _____.__.__                  '
@@ -21,10 +22,34 @@ log() {
     echo "==> $1";
 }
 
+install_chrome() {
+    if [[ ! $INSTALLED_CASK_FORMULAS =~ google-chrome ]]; then
+        log "Installing chrome..."
+        brew cask install google-chrome;
+        log "chrome installed"
+    fi
+}
+
+install_intellij() {
+    if [[ ! $INSTALLED_CASK_FORMULAS =~ intellij-idea-ce ]]; then
+        log "Installing IntelliJ..."
+        brew cask install intellij-idea-ce;
+        log "IntelliJ installed"
+    fi
+}
+
+install_alfred() {
+    if [[ ! $INSTALLED_CASK_FORMULAS =~ alfred ]]; then
+        log "Installing alfred..."
+        brew cask install alfred;
+        log "alfred installed"
+    fi
+}
+
 install_slate() {
-    if [[ ! -d /Applications/Slate.app ]]; then
+    if [[ ! $INSTALLED_CASK_FORMULAS =~ slate ]]; then
         log "Installing slate..."
-        curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz -C /Applications
+        brew cask install slate;
         log "slate installed"
     fi
 }
@@ -40,7 +65,7 @@ install_tmux() {
 install_zsh() {
     if [[ ! $INSTALLED_FORMULAS =~ zsh ]]; then
         log "Installing zsh..."
-        brew install zsh >/dev/null;
+        brew install zsh;
         log "zsh installed"
     fi
 }
@@ -48,8 +73,24 @@ install_zsh() {
 install_git() {
     if [[ ! $INSTALLED_FORMULAS =~ git ]]; then
         log "Installing git..."
-        brew install git >/dev/null;
+        brew install git;
         log "git installed"
+    fi
+}
+
+install_jenv() {
+    if [[ ! $INSTALLED_FORMULAS =~ jenv ]]; then
+        log "Installing jenv..."
+        brew install jenv;
+        log "jenv installed"
+    fi
+}
+
+install_rbenv() {
+    if [[ ! $INSTALLED_FORMULAS =~ rbenv ]]; then
+        log "Installing rbenv..."
+        brew install rbenv;
+        log "rbenv installed"
     fi
 }
 
@@ -64,10 +105,17 @@ install_cask() {
 
 install() {
     log "Installing..."
-    install_zsh
+    # brew backages
     install_git
+    install_zsh
     install_tmux
     install_cask
+    install_jenv
+    # brew cask packages
+    install_alfred
+    install_slate
+    install_intellij
+    install_chrome
     log "Installation finished"
 }
 
