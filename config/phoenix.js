eae04546eb46813ff8b1b84105cd7e36ccacd0c3
Tomas Ave <tomave82@gmail.com>
2016-06-16 09:21:04 +0200

    Added basic phoenix config for moving windows

--- config/phoenix.js
+++ config/phoenix.js
@@ -9,10 +9,45 @@ function debug(message) {
 };
 
 
-keys.push(Phoenix.bind('j', hyper, function() {
+// move window left
+keys.push(Phoenix.bind('h', hyper, function() {
   var window = Window.focusedWindow();
   if (!window) return;
-  window.setSize(
-      {width: 50.0, height: 50.0}
-      );
+  var screen = window.screen();
+  var frame = screen.visibleFrameInRectangle();
+  var size = {
+    width: frame.width/2, 
+    height: frame.heigt
+  };
+  var pos = {
+    x: 0,
+    y: 0
+  };
+  window.setSize(size);
+  window.setTopLeft(pos);
+}));
+
+// move window right
+keys.push(Phoenix.bind('l', hyper, function() {
+  var window = Window.focusedWindow();
+  if (!window) return;
+  var screen = window.screen();
+  var frame = screen.visibleFrameInRectangle();
+  var size = {
+    width: Math.round(frame.width/2), 
+    height: frame.heigt
+  };
+  var pos = {
+    x: Math.round(frame.width/2),
+    y: 0
+  };
+  window.setSize(size);
+  window.setTopLeft(pos);
+}));
+
+// maximize window
+keys.push(Phoenix.bind('k', hyper, function() {
+  var window = Window.focusedWindow();
+  if (!window) return;
+  window.maximize();
 }));
