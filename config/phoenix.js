331609de69510983569044cb499a33ec1f48ab3e
Tomas Ave <cod3hulk@gmail.com>
2016-06-17 09:17:18 +0200

    Added multi window support for phoenix

--- config/phoenix.js
+++ config/phoenix.js
@@ -1,5 +1,9 @@
-var hyper = ['alt', 'cmd'];
+var hyper = ['ctrl', 'cmd'];
 var keys = [];
+
+Phoenix.set({
+  'openAtLogin': true
+});
 
 const DEBUG = true;
 function debug(message) {
@@ -20,17 +24,35 @@ keys.push(Phoenix.bind('h', hyper, function() {
     height: frame.heigt
   };
   var pos = {
-    x: 0,
-    y: 0
+    x: frame.x,
+    y: frame.y
   };
-  window.setSize(size);
+
+  // recompute size and position for next screen
+  var curr_pos = window.topLeft();
+  if (curr_pos.x == pos.x && curr_pos.y == pos.y) {
+    var prev_screen = screen.previous();
+    var frame = prev_screen.visibleFrameInRectangle();
+    size = {
+      width: Math.round(frame.width/2), 
+      height: frame.heigt
+    };
+    pos = {
+      x: frame.x + Math.round(frame.width/2),
+      y: frame.y
+    };
+  }
+
   window.setTopLeft(pos);
+  window.setSize(size);
 }));
 
 // move window right
 keys.push(Phoenix.bind('l', hyper, function() {
   var window = Window.focusedWindow();
   if (!window) return;
+
+  // compute size and position
   var screen = window.screen();
   var frame = screen.visibleFrameInRectangle();
   var size = {
@@ -38,11 +60,28 @@ keys.push(Phoenix.bind('l', hyper, function() {
     height: frame.heigt
   };
   var pos = {
-    x: Math.round(frame.width/2),
-    y: 0
+    x: frame.x + Math.round(frame.width/2),
+    y: frame.y
   };
-  window.setSize(size);
+
+  // recompute size and position for next screen
+  var curr_pos = window.topLeft();
+  if (curr_pos.x == pos.x && curr_pos.y == pos.y) {
+    var next_screen = screen.next();
+    var frame = next_screen.visibleFrameInRectangle();
+    size = {
+      width: frame.width/2, 
+      height: frame.heigt
+    };
+    pos = {
+      x: frame.x,
+      y: frame.y
+    };
+  }
+
+
   window.setTopLeft(pos);
+  window.setSize(size);
 }));
 
 // maximize window
@@ -51,3 +90,4 @@ keys.push(Phoenix.bind('k', hyper, function() {
   if (!window) return;
   window.maximize();
 }));
+
