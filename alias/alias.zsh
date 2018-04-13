8911792e1c68a1f7b5a5f0c63667c2242ae5bfa1
Tomas Ave <cod3hulk@gmail.com>
2018-04-13 11:48:25 +0200

    Add alias for setting up PR and edit aliases

--- alias/alias.zsh
+++ alias/alias.zsh
@@ -6,3 +6,7 @@ alias vim='nvim'
 
 # git
 alias gcob='gco $(git branch -r | cut -d "/" -f2- | fzf)'
+alias gcp='git create-pull-request'
+
+# edit alias
+alias ea="vi ${HOME}/dotfiles/alias/alias.zsh"
