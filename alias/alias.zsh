f8894cccb8a427fc33c53fbb056e025839ff4f9c
Tomas Ave <tomave82@gmail.com>
2018-05-11 22:57:45 +0200

    Use emacsclient for editing aliases

--- alias/alias.zsh
+++ alias/alias.zsh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+# emacs
+alias e='emacsclient -t -a ""'
+alias ec='emacsclient -c -n -a ""'
+
 # neovim
 alias vi='nvim'
 alias vim='nvim'
@@ -9,7 +13,7 @@ alias gcob='gco $(git branch -r | cut -d "/" -f2- | fzf)'
 alias gcp='git create-pull-request'
 
 # edit alias
-alias ea="vi ${HOME}/dotfiles/alias/alias.zsh"
+alias aliases="e ${HOME}/dotfiles/alias/alias.zsh"
 
 # mvn
 alias mc='mvn clean'
@@ -18,6 +22,3 @@ alias mci='mc install'
 alias mt='mvn test'
 alias mi='mvn install'
 
-# emacs
-alias e='emacsclient -t -a ""'
-alias ec='emacsclient -c -n -a ""'
