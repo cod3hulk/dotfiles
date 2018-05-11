74748ecdd5899739eeb6cddb68655b68bf2bbb7b
Tomas Ave <tomave82@gmail.com>
2018-05-12 00:06:03 +0200

    Add custom mpaaing for evil save and close

--- spacemacs/spacemacs.el
+++ spacemacs/spacemacs.el
@@ -312,6 +312,10 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+
+  (spacemacs/set-leader-keys "fx" 'evil-save-modified-and-close)
+
+
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
