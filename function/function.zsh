da375fdcea301a090ba7bf1816f564ee9e134daf
cod3hulk <cod3hulk@gmail.com>
2018-07-31 14:29:30 +0200

    Remove debug messages

--- function/function.zsh
+++ function/function.zsh
@@ -19,30 +19,23 @@ function ij() {
 
 function open_with_app() {
     # were we given a file?
-    echo "$1"
     if [ -f "$1" ]; then
-        #  echo "opening '$1'"
         open -a "$IDEA" "$1"
     else
         # does our working dir have an .idea directory?
         if [ -d ".idea" ]; then
-            echo foo
-            #      echo "opening via the .idea dir"
             open -a "$IDEA" .
 
         # is there an IDEA project file?
         elif [ -f "*.ipr" ]; then
-            #      echo "opening via the project file"
             open -a "$IDEA" `ls -1d *.ipr | head -n1`
 
         # Is there a pom.xml?
         elif [ -f "pom.xml" ]; then
-            #      echo "importing from pom"
             open -a "$IDEA" "pom.xml"
 
         # can't do anything smart; just open IDEA
         else
-            #      echo 'cbf'
             open "$IDEA"
         fi
     fi
