e1d37100ab9f44446d4bdbc6f63076213eaa9696
cod3hulk <cod3hulk@gmail.com>
2018-10-18 09:59:49 +0200

    Add support for fzf support for changing directories

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -1,2 +1,8 @@
 #!/bin/zsh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# interative cd
+if [[ -s "${ZDOTDIR:-$HOME}/dotfiles/fzf/interactive-cd.zsh"  ]]; then
+    source "${ZDOTDIR:-$HOME}/dotfiles/fzf/interactive-cd.zsh"
+fi
+
