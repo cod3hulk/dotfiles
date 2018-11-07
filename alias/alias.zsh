6ea28f71980503a59bc54fe009f060a399319262
cod3hulk <cod3hulk@gmail.com>
2018-11-07 23:17:17 +0100

    Add new aliases

--- alias/alias.zsh
+++ alias/alias.zsh
@@ -13,15 +13,20 @@ alias gcob='gco $(git branch -r | cut -d "/" -f2- | fzf)'
 alias gcp='git create-pull-request'
 alias gpc='git push -u origin "$(git-branch-current 2> /dev/null)"'
 
+# docker
+alias compose='docker-compose'
+
 # edit alias
-alias aliases="e ${HOME}/dotfiles/alias/alias.zsh"
+alias aliases="vi ${HOME}/dotfiles/alias/alias.zsh"
 
 # mvn
 alias mc='mvn clean'
 alias mct='mc test'
 alias mci='mc install'
+alias mcp='mc package'
 alias mt='mvn test'
 alias mi='mvn install'
+alias mp='mvn package'
 
 # local aliases
 source "${HOME}/.aliases.local"
