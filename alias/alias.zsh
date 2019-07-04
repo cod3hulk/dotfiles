bd2451ae7d93de23fa5526636bf7a6f1a094bf23
Tomas Ave <cod3hulk@gmail.com>
2019-07-04 09:55:28 +0200

    Update kubernetes aliases

--- alias/alias.zsh
+++ alias/alias.zsh
@@ -32,16 +32,10 @@ alias mi='mvn install'
 alias mp='mvn package'
 
 # kubernetes
-alias klogs='kubectl get pods --no-headers -o custom-columns=":metadata.name" | fzf | xargs kubectl logs -f'
-alias kpods='kubectl get pods'
-function ktail() {
-  SINCE=${1:-10s}
-  SERVICE_NAME=`kubectl get pods --no-headers -o custom-columns=":metadata.name" | sed 's/-[a-z0-9]*-[a-z0-9]*-[a-z0-9]*$//g' | uniq | fzf`
-  kubetail $SERVICE_NAME -s $SINCE -k pod
-}
-alias kctx='kubectx'
+alias kc='kubectx'
+alias kl='kubectl get pods --no-headers -o custom-columns=":metadata.name" | sed "s/-[a-z0-9]*-[a-z0-9]*-[a-z0-9]*$//g" | uniq | fzf | xargs stern'
+alias kp='kubectl get pods'
 alias k='kubectl'
-
 
 # local aliases
 source "${HOME}/.aliases.local"
