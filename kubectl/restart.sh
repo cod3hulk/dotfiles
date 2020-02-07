c2ece96f0abe76990b45c9a29abc1c28a725955e
Tomas Ave <cod3hulk@gmail.com>
2020-02-07 08:42:33 +0100

    Update kubectl restart cmd

--- kubectl/restart.sh
+++ kubectl/restart.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  deployment="$(kubectl get deployments --no-headers -o custom-columns=':metadata.name' | sort -u | fzf)"
-  kubectl rollout restart deployment "$deployment"
-else
-  kubectl rollout restart deployment "$1"
-fi
+FZF_CMD="fzf"
+[ ! -z "$1" ] && FZF_CMD="$FZF_CMD -q $1"
+DEPLOYMENT="$(kubectl get deployments --no-headers -o custom-columns=':metadata.name' | sort -u | $FZF_CMD)"
+kubectl rollout restart deployment "$DEPLOYMENT"
