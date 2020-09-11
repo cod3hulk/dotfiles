54534d266089d78fdc21b5f77890f22a644c8bba
Tomas Ave <cod3hulk@gmail.com>
2020-09-11 14:59:55 +0200

    Add support for debian based system

--- autojump/init.zsh
+++ autojump/init.zsh
@@ -1,2 +1,8 @@
-#!/bin/zsh
-[[ -s $(brew --prefix)/etc/profile.d/autojump.sh  ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+#!/bin/bash
+if [ `uname` = 'Linux' ]
+  [[ -s /usr/share/autojump/autojump.sh ]] && source /usr/share/autojump/autojump.sh
+then
+elif [ `uname` = 'Darwing' ]
+then
+  [[ -s $(brew --prefix)/etc/profile.d/autojump.sh  ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+fi
