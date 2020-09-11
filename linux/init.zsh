3842a7bf3584f1cc86a0285619eeb8c62ac88917
Tomas Ave <cod3hulk@gmail.com>
2020-09-11 15:37:29 +0200

    Add support for Debian packages

--- linux/init.zsh
+++ linux/init.zsh
@@ -1,5 +1,20 @@
 #!/bin/bash
 if [ `uname` == 'Linux' ]
 then
-  sudo apt install chromium-browser
+  sudo apt install -y \
+    chromium-browser \
+    tmux \
+    zsh \
+    zgen \
+    python \
+    python3 \
+    neovim \
+    python3-neovim \
+    curl \
+    autojump \
+    silversearcher-ag \
+    docker.io \
+    docker-compose \
+    openjdk-14-jdk-headless \
+    tig
 fi
