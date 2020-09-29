89e4e9286e4ab99a3a4b7191ec25ebdac01653ac
Tomas Ave <cod3hulk@gmail.com>
2020-09-29 16:10:17 +0200

    Update to new dotfiles strucutre

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -1,8 +1,10 @@
 #!/bin/zsh
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+[ `uname` = 'Darwin' ] && [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+[ `uname` = 'Linux' ] && [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh 
+[ `uname` = 'Linux' ] && [ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh 
 
 # interative cd
-if [[ -s "${ZDOTDIR:-$HOME}/dotfiles/fzf/interactive-cd.zsh"  ]]; then
-    source "${ZDOTDIR:-$HOME}/dotfiles/fzf/interactive-cd.zsh"
+if [[ -s "${ZDOTDIR:-$HOME}/.dotfiles/fzf/interactive-cd.zsh"  ]]; then
+    source "${ZDOTDIR:-$HOME}/.dotfiles/fzf/interactive-cd.zsh"
 fi
 
