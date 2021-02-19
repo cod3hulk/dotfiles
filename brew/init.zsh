fa76b9c776fbcbb8c555254daac1942c47dc86a7
Tomas Ave <cod3hulk@gmail.com>
2021-02-19 23:04:05 +0100

    Fix new dotfiles location

--- brew/init.zsh
+++ brew/init.zsh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BREW_FILE="${HOME}/dotfiles/brew/Brewfile"
+BREW_FILE="${HOME}/.dotfiles/brew/Brewfile"
 if [ -e ${BREW_FILE} ] && [ `uname` == 'Darwin' ]
 then
   brew bundle install --file=${BREW_FILE}
