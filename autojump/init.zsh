1aa2a773fba0992e7e64cc53ae375805c623683b
Tomas Ave <cod3hulk@gmail.com>
2021-02-19 23:03:22 +0100

    Fix typo in autojump config

--- autojump/init.zsh
+++ autojump/init.zsh
@@ -2,7 +2,7 @@
 if [ `uname` = 'Linux' ]
   [[ -s /usr/share/autojump/autojump.sh ]] && source /usr/share/autojump/autojump.sh
 then
-elif [ `uname` = 'Darwing' ]
+elif [ `uname` = 'Darwin' ]
 then
   [[ -s $(brew --prefix)/etc/profile.d/autojump.sh  ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
 fi
