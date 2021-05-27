9daf5fd7eee992c37dbd81f19e21e9a764f42b1e
Tomas Ave <cod3hulk@gmail.com>
2021-05-27 09:01:30 +0200

    Make brew installer workable for arm64

--- brew/init.zsh
+++ brew/init.zsh
@@ -2,5 +2,5 @@
 BREW_FILE="${HOME}/.dotfiles/brew/Brewfile"
 if [ -e ${BREW_FILE} ] && [ `uname` == 'Darwin' ]
 then
-  brew bundle install --file=${BREW_FILE}
+  arch -arm64 brew bundle install --file=${BREW_FILE}
 fi
