11d08ed94e30dfe2c0a4eac93c8ad54438b8b9f9
Tomas Ave <cod3hulk@gmail.com>
2021-11-30 11:15:34 +0100

    Add limelight config

---
+++ limelight/limelight.sh
@@ -0,0 +1,5 @@
+${HOME}/bin/limelight -m config active_color 0xffff79c6
+${HOME}/bin/limelight -m config width 5
+${HOME}/bin/limelight -m config placement inset
+${HOME}/bin/limelight -m config radius 0.0
+
