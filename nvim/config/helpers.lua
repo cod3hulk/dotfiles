7de1c5e59e5612c3fe5a485d248ddb67cb048762
Tomas Ave <cod3hulk@gmail.com>
2022-03-08 23:01:06 +0100

    Migrate common vim to lua based config

---
+++ nvim/config/helpers.lua
@@ -0,0 +1,9 @@
+cmd = vim.cmd
+opt = vim.opt
+g = vim.g
+
+function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
---
+++ nvim/lua/common.lua
@@ -0,0 +1,33 @@
+require("helpers")
+---------------------- Mappings ---------------------- 
+g.mapleader = ' '
+map('n', '<leader>w', ':update<CR>')
+map('i', 'fd', '<ESC>')
+map('n', '<CR>', ':nohlsearch<CR>')
+map('n', '<leader>q', ':q<cr>')
+map('n', '<leader>x', ':x<cr>')
+
+---------------------- General ---------------------- 
+opt.number = true
+opt.relativenumber = true
+opt.clipboard ='unnamedplus'
+opt.ignorecase = true
+opt.expandtab = true
+opt.smarttab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.incsearch = true
+opt.hls = true
+opt.scrolloff = 10
+
+-- cmd 'colorscheme dracula'
+
+--[[
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Diff colors
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+highlight DiffAdd    cterm=bold ctermfg=10 ctermbg=17 gui=none guifg=bg guibg=Red
+highlight DiffDelete cterm=bold ctermfg=10 ctermbg=17 gui=none guifg=bg guibg=Red
+highlight DiffChange cterm=bold ctermfg=10 ctermbg=17 gui=none guifg=bg guibg=Red
+highlight DiffText   cterm=bold ctermfg=10 ctermbg=88 gui=none guifg=bg guibg=Red
+]]
