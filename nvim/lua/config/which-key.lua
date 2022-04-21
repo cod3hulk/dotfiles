76bbfe28acf8b44454ac7260daf7ac0566c3f99f
Tomas Ave <cod3hulk@gmail.com>
2022-04-21 09:41:29 +0200

    Add addtional which-key bindings

--- nvim/lua/config/which-key.lua
+++ nvim/lua/config/which-key.lua
@@ -9,4 +9,7 @@ wk.register({
     g = { "<cmd>Telescope live_grep<cr>", "Grep File"}, 
     n = { "<cmd>:new<cr>", "New File" }, 
   },
+  w = { "<cmd>:w<cr>", "Write File" },
+  x = { "<cmd>:x<cr>", "Write And Close File" },
+  q = { "<cmd>:q<cr>", "Close File" }
 }, { prefix = "<leader>" })
