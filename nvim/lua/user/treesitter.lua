349110a92bd95fba0b4026d811ed1db877c3b365
Tomas Ave <cod3hulk@gmail.com>
2022-09-16 10:50:59 +0200

    Fix issue with treesitter and phpdoc

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -6,7 +6,7 @@ end
 configs.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
+  ignore_install = { "phpdoc", "tree-sitter-phpdoc" }, -- List of parsers to ignore installing
   autopairs = {
     enable = true,
   },
@@ -15,7 +15,7 @@ configs.setup {
     disable = { "" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
-  indent = { enable = true, disable = { "yaml" } },
+  indent = { enable = true, disable = { "" } },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
