82ff3b1ffa9b17616b933a34d23d55717184e740
Tomas Ave <cod3hulk@gmail.com>
2022-09-22 14:06:57 +0200

    Use mason instead of lsp-installer

---
+++ nvim/lua/user/lsp/mason.lua
@@ -0,0 +1,49 @@
+local status_ok, mason = pcall(require, "mason")
+if not status_ok then
+  print "unable to load mason"
+  return
+end
+
+local status_ok_1, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not status_ok_1 then
+  print "unable to load mason-lspconfig"
+  return
+end
+
+local servers = {
+  "sumneko_lua",
+  "kotlin_language_server",
+}
+
+mason.setup()
+
+mason_lspconfig.setup {
+  ensure_installed = servers,
+  automatic_installation = true,
+}
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  print "unable to lspconfig"
+  return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+  opts = {
+    on_attach = require("user.lsp.handlers").on_attach,
+    capabilities = require("user.lsp.handlers").capabilities,
+  }
+
+  server = vim.split(server, "@")[1]
+
+
+  if server == "sumneko_lua" then
+    local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  end
+
+
+  lspconfig[server].setup(opts)
+end
