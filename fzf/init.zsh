3461f15a53a0db5d4d501ab673c7d08b439ba83f
Tomas Ave <cod3hulk@gmail.com>
2025-01-29 09:11:27 +0100

    Adjust init script for fzf

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -1,7 +1,5 @@
 #!/bin/zsh
-[ `uname` = 'Darwin' ] && [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-[ `uname` = 'Linux' ] && [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh 
-[ `uname` = 'Linux' ] && [ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh 
+(( $+commands[fzf] )) && source <(fzf --zsh)
 
 # interative cd
 if [[ -s "${ZDOTDIR:-$HOME}/.dotfiles/fzf/interactive-cd.zsh"  ]]; then
