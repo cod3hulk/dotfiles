e4f89a5e3670f04915031130b2c84af14fbdba96
Tomas Ave <cod3hulk@gmail.com>
2025-06-24 16:19:54 +0200

    Make ctrl-d and ctrl-u have the cursor in the middle

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -19,6 +19,8 @@ keymap('n', '<C-h>', '<C-w>h', opts)
 keymap('n', '<C-j>', '<C-w>j', opts)
 keymap('n', '<C-k>', '<C-w>k', opts)
 keymap('n', '<C-l>', '<C-w>l', opts)
+keymap('n', '<C-d>', '<C-d>zz', opts)
+keymap('n', '<C-u>', '<C-u>zz', opts)
 
 -- move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
