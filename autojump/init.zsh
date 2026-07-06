b47e033e2c07149a4dcdc1955b849fb20523902e
Tomas Ave <cod3hulk@gmail.com>
2026-07-06 19:50:07 +0200

    Fix autojump config to speed up shell startup

--- autojump/init.zsh
+++ autojump/init.zsh
@@ -1,8 +1,11 @@
-#!/bin/bash
-if [ `uname` = 'Linux' ]
-  [[ -s /usr/share/autojump/autojump.sh ]] && source /usr/share/autojump/autojump.sh
-then
-elif [ `uname` = 'Darwin' ]
-then
-  [[ -s $(brew --prefix)/etc/profile.d/autojump.sh  ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+#!/bin/zsh
+# Hardcode Homebrew prefixes to avoid forking `brew --prefix` on every startup.
+if [[ "$OSTYPE" == linux* ]]; then
+    [[ -s /usr/share/autojump/autojump.sh ]] && source /usr/share/autojump/autojump.sh
+elif [[ "$OSTYPE" == darwin* ]]; then
+    if [[ -s /opt/homebrew/etc/profile.d/autojump.sh ]]; then
+        source /opt/homebrew/etc/profile.d/autojump.sh
+    elif [[ -s /usr/local/etc/profile.d/autojump.sh ]]; then
+        source /usr/local/etc/profile.d/autojump.sh
+    fi
 fi
